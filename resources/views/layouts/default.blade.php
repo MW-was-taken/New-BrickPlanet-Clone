7203f114c3b47ff5a7de9d49c32844f210f956d6
Hurricane0879 <timemachinecorey@gmail.com>
2023-08-26 23:27:36 -0600

    added header and sidebar for guest (not logged in yet) also active tabs for current page on sidebar!

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -28,280 +28,106 @@
     @yield('css')
 </head>
 <body>
-    <nav class="navbar navbar-expand-md fixed-top">
-        <button class="navbar-toggler" id="sidebarToggler" style="border:none;" type="button">
-            <i class="fas fa-bars" style="font-size: 23px;"></i>
-        </button>
-
-        <a href="{{ (Auth::check()) ? route('home.dashboard') : route('home.index') }}" class="navbar-brand">
-            <img src="{{ config('site.logo') }}" width="150px">
-        </a>
-
-        @guest
-            <div class="nav-item show-sm-only"></div>
-        @else
-            <div class="nav-item dropdown headshot show-sm-only">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <img src="{{ Auth::user()->headshot() }}" width="40px">
+    @guest
+    <nav class="navbar">
+        <ul class="navbar-nav me-auto flex-grow-1">
+            <li class="nav-item d-flex d-lg-none pe-2">
+                <a type="button" class="nav-link" id="mobile-dropdown"><i class="far fa-bars text-2xl lh-1 align-middle"></i></a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ env('APP_LINK') }}" class="nav-link nav-brand p-0" style="padding:0">
+                    <img src="/assets/images/bp-primary1.png" width="200" class="d-none d-lg-block">
+                    <img src="/assets/images/bp-light.png" width="54" height="54" class="d-block d-lg-none">
                 </a>
-                <div class="dropdown-menu">
-                    <a href="{{ route('users.profile', Auth::user()->username) }}" class="dropdown-item">
-                        <i class="fas fa-user"></i>
-                        <span>Profile</span>
-                    </a>
-
-                    @if (site_setting('character_enabled'))
-                        <a href="{{ route('account.character.index') }}" class="dropdown-item">
-                            <i class="fas fa-tshirt"></i>
-                            <span>Character</span>
-                        </a>
-                    @endif
-
-                    @if (site_setting('settings_enabled'))
-                        <a href="{{ route('account.settings.index', '') }}" class="dropdown-item">
-                            <i class="fas fa-wrench"></i>
-                            <span>Settings</span>
-                        </a>
-                    @endif
-
-                    <a href="{{ route('auth.logout') }}" class="dropdown-item">
-                        <i class="fas fa-sign-out-alt"></i>
-                        <span>Logout</span>
-                    </a>
-                </div>
-            </div>
-        @endguest
-
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav mr-auto" style="width:50%;">
-                <span><i class="fas fa-search"></i></span>
-                <input class="navbar-search" id="navbarSearch" placeholder="Search for users, items and groups...">
-            </ul>
-
-            <ul class="navbar-nav ml-auto">
-                @guest
-                    <li class="nav-item hide-sm">
-                        <a href="{{ route('auth.login.index') }}" class="nav-link">
-                            <i class="fas fa-user"></i>
-                            <span>Login</span>
-                        </a>
-                    </li>
-                    @if (site_setting('registration_enabled'))
-                        <li class="nav-item hide-sm">
-                            <a href="{{ route('auth.register.index') }}" class="nav-link">
-                                <i class="fas fa-user-plus"></i>
-                                <span>Register</span>
-                            </a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item">
-                        <a href="{{ route('account.money.index', '') }}" class="nav-link" title="{{ str_replace('from now', '', Auth::user()->next_currency_payout->diffForHumans()) }} until next reward" data-toggle="tooltip">
-                            <i class="currency"></i>
-                            <span>{{ number_format(Auth::user()->currency) }}</span>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown headshot hide-sm">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="{{ Auth::user()->headshot() }}" width="40px">
-                        </a>
-                        <div class="dropdown-menu" style="margin-left:-100px;">
-                            <a href="{{ route('users.profile', Auth::user()->username) }}" class="dropdown-item">
-                                <i class="fas fa-user"></i>
-                                <span>Profile</span>
-                            </a>
-
-                            @if (site_setting('character_enabled'))
-                                <a href="{{ route('account.character.index') }}" class="dropdown-item">
-                                    <i class="fas fa-tshirt"></i>
-                                    <span>Character</span>
+            </li>
+            <li class="nav-item flex-grow-1 px-3 d-lg-block d-none ">
+                <form autocomplete="off" class="flex-grow-1" action="" method="get">
+                    <main class="flex-grow-1 form-parent has-icon-right">
+                        <input autocomplete="off" type="text" class="form" id="searchDropdown" data-bs-toggle="dropdown" aria-expanded="false" name="search" placeholder="Search {{ env('APP_NAME') }}..." value="">
+                        <ul class="dropdown-menu w-100" aria-labelledby="searchDropdown" data-bs-popper="static">
+                            <li class="dropdown-item">
+                                <a id="searchGamesBtn" class="dropdown-link">
+                                    <i class="far fa-gamepad-alt dropdown-icon"></i>&nbsp;Search Games for '<span></span>'
                                 </a>
-                            @endif
-
-                            @if (site_setting('settings_enabled'))
-                                <a href="{{ route('account.settings.index', '') }}" class="dropdown-item">
-                                    <i class="fas fa-wrench"></i>
-                                    <span>Settings</span>
-                                </a>
-                            @endif
-
-                            <a href="{{ route('auth.logout') }}" class="dropdown-item">
-                                <i class="fas fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+                            </li>
+                        </ul>
+                        <button type="submit" class="form-parent-icon far fa-search"></button>
+                    </main>
+                </form>
+            </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item gap-2">
+                <a href="{{ env('APP_LINK') }}/login" class="nav-link btn btn-success fw-semibold text-uppercase px-3 text-sm flex-shrink">Log In</a>
+                <a href="{{ env('APP_LINK') }}/register" class="nav-link btn btn-primary fw-semibold text-uppercase px-3 text-sm flex-shrink">Play Now</a>
+            </li>
+        </ul>
     </nav>
-    <div class="navbar-search-dropdown-parent">
-        <div class="navbar-search-dropdown" id="navbarSearchResults" style="display:none;"></div>
-    </div>
-
-    <nav class="sidebar">
-        <div class="mb-2"></div>
-        <div class="mt-2 show-sm-only"></div>
-        <div class="show-sm-only" style="padding-left: 5px; padding-right: 5px;">
-            <form action="{{ route('users.index', '') }}" method="GET">
-                <input class="form-control" style="height: 38px;" type="text" name="search" placeholder="Search Avasquare..." @if (request()->route()->getName() == 'users.index') value="{{ request()->search }}" @endif>
-            </form>
-        </div>
-        <div class="mb-1 show-sm-only"></div>
-        @guest
-            <div class="show-sm-only">
-                <div class="section-div">AUTH</div>
-                <a href="{{ route('auth.login.index') }}">
-                    <i class="fas fa-user sidebar-icon"></i>
-                    <span class="sidebar-text">Login</span>
-                </a>
-                @if (site_setting('registration_enabled'))
-                    <a href="{{ route('auth.register.index') }}">
-                        <i class="fas fa-user-plus sidebar-icon"></i>
-                        <span class="sidebar-text">Register</span>
-                    </a>
-                @endif
-            </div>
-        @else
-            <div class="show-sm-only">
-                <a href="{{ route('account.money.index', '') }}">
-                    <i class="currency"></i>
-                    <span class="sidebar-text">{{ number_format(Auth::user()->currency) }} Currency</span>
-                </a>
-            </div>
-        @endguest
-        <div class="section-div">NAVIGATION</div>
-        <a href="{{ (Auth::check()) ? route('home.dashboard') : route('home.index') }}">
-            <i class="fas fa-house-user sidebar-icon"></i>
-            <span class="sidebar-text">Home</span>
-        </a>
-        <a href="{{ route('catalog.index') }}">
-            <i class="fas fa-store sidebar-icon"></i>
-            <span class="sidebar-text">Catalog</span>
-        </a>
-
-        @if (site_setting('forum_enabled'))
-            <a href="{{ route('forum.index') }}">
-                <i class="fas fa-comment-alt sidebar-icon"></i>
-                <span class="sidebar-text">Forum</span>
-            </a>
-        @endif
-
-        @if (site_setting('groups_enabled'))
-            <a href="{{ route('groups.index') }}">
-                <i class="fas fa-building sidebar-icon"></i>
-                <span class="sidebar-text">Groups</span>
-            </a>
-        @endif
-
-        <a href="{{ route('users.index', '') }}">
-            <i class="fas fa-telescope sidebar-icon"></i>
-            <span class="sidebar-text">Search</span>
-        </a>
-
-        @auth
-            <a href="{{ route('account.promocodes.index') }}">
-                <i class="fas fa-ticket-alt sidebar-icon"></i>
-                <span class="sidebar-text">Promocodes</span>
-            </a>
-            @if (site_setting('real_life_purchases_enabled'))
-                <a href="{{ route('account.upgrade.index') }}">
-                    <i class="fas fa-rocket sidebar-icon"></i>
-                    <span class="sidebar-text">Upgrade</span>
-                </a>
-            @endif
-            <div class="section-div">PERSONAL</div>
-            <a href="{{ route('account.money.index', '') }}">
-                <i class="fas fa-money-bill-alt sidebar-icon"></i>
-                <span class="sidebar-text">Money</span>
-            </a>
-            <a href="{{ route('account.friends.index') }}">
-                <i class="fas fa-user-friends sidebar-icon"></i>
-                <span class="sidebar-text">Friends</span>
-                @if (Auth::user()->friendRequestCount() > 0)
-                    <span class="notification float-right">{{ number_format(Auth::user()->friendRequestCount()) }}</span>
-                @endif
-            </a>
-            <a href="{{ route('account.inbox.index', '') }}">
-                <i class="fas fa-inbox sidebar-icon"></i>
-                <span class="sidebar-text">Inbox</span>
-                @if (Auth::user()->messageCount() > 0)
-                    <span class="notification float-right">{{ number_format(Auth::user()->messageCount()) }}</span>
-                @endif
-            </a>
-            @if (site_setting('trading_enabled'))
-                <a href="{{ route('account.trades.index', '') }}">
-                    <i class="fas fa-exchange sidebar-icon"></i>
-                    <span class="sidebar-text">Trades</span>
-                    @if (Auth::user()->tradeCount() > 0)
-                        <span class="notification float-right">{{ number_format(Auth::user()->tradeCount()) }}</span>
-                    @endif
-                </a>
-            @endif
-
-            @if (Auth::user()->isStaff())
-                <a href="{{ route('admin.index') }}">
-                    <i class="fas fa-gavel sidebar-icon"></i>
-                    <span class="sidebar-text">Panel</span>
-                    @if (pendingAssetsCount() > 0 || pendingReportsCount() > 0)
-                        <span class="notification float-right">
-                            @if (pendingAssetsCount() > 0)
-                                <span>(A: {{ number_format(pendingAssetsCount()) }})</span>
-                            @endif
-
-                            @if (pendingReportsCount() > 0)
-                                <span>(R: {{ number_format(pendingReportsCount()) }})</span>
-                            @endif
-                        </span>
-                    @endif
-                </a>
-            @endif
-        @endauth
+@if (!request()->is('/'))
+<style>
+      @media (min-width: 993px) {
+        .wrapper {
+          padding-left: 220px;
+        }
+      }
+      </style>
+<nav class="sidebar " style="overflow-y:auto">
+      <ul class="sidebar-nav">
+        <main class="divider">Browse</main>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/login" class="sidebar-link @if (request()->is('login')) sidebar-link-active @endif">
+            <i class="fas fa-house-chimney sidebar-icon text-xl"></i>
+            Home
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/games" class="sidebar-link @if (request()->is('games')) sidebar-link-active @endif">
+            <i class="fas fa-gamepad-alt sidebar-icon text-xl"></i>
+            Games
+          </a>
+        </li>
+        <li class="sidebar-item">
+<a href="{{ env('APP_LINK') }}/shop" class="sidebar-link @if (request()->is('catalog')) sidebar-link-active @endif">
+          <i class="fas fa-shopping-basket sidebar-icon text-xl"></i>
+            Shop
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/forum" class="sidebar-link @if (request()->is('forum')) sidebar-link-active @endif">
+            <i class="fas fa-messages sidebar-icon text-xl"></i>
+            Forum
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/upgrade" class="sidebar-link text-warning @if (request()->is('upgrade')) sidebar-link-active text-warning @endif">
+            <i class="fas fa-crown sidebar-icon text-warning text-xl"></i>
+            Upgrade
+          </a>
+        </li>
+                <main class="divider">Discover</main>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/players" class="sidebar-link @if (request()->is('players')) sidebar-link-active @endif">
+            <i class="fas fa-users sidebar-icon text-xl"></i>
+            Players
+          </a>
+        </li>
+        
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/guilds" class="sidebar-link @if (request()->is('guilds')) sidebar-link-active @endif">
+            <i class="fas fa-swords sidebar-icon text-xl"></i>
+            Guilds
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <a href="{{ env('APP_LINK') }}/economy" class="sidebar-link @if (request()->is('/economy')) sidebar-link-active @endif">
+            <i class="fas fa-chart-mixed sidebar-icon text-xl"></i>
+            Economy
+          </a>
+        </li>
+              </ul>
     </nav>
-
-    <div class="container-custom">
-        @if (site_setting('alert_enabled') && site_setting('alert_message'))
-            <div class="alert alert-site text-center mb-4" style="background:{{ site_setting('alert_background_color') }};color:{{ site_setting('alert_text_color') }};">
-                <div class="row">
-                    <div class="col-1 align-self-center pl-1 pr-1">
-                        <i class="fas fa-exclamation-circle"></i>
-                    </div>
-                    <div class="col-10 align-self-center pl-1 pr-1">
-                        <strong style="word-wrap:break-word;">{!! site_setting('alert_message') !!}</strong>
-                    </div>
-                    <div class="col-1 align-self-center pl-1 pr-1">
-                        <i class="fas fa-exclamation-circle"></i>
-                    </div>
-                </div>
-            </div>
-        @endif
-
-        @if (site_setting('maintenance_enabled'))
-            <div class="alert bg-danger text-center text-white">
-                You are currently in maintenance mode. <a href="{{ route('maintenance.exit') }}" class="text-white" style="font-weight:600;">[Exit]</a>
-            </div>
-        @endif
-
-        @if (count($errors) > 0)
-            <div class="alert bg-danger text-white">
-                @foreach ($errors->all() as $error)
-                    <div>{!! $error !!}</div>
-                @endforeach
-            </div>
-        @endif
-
-        @if (session()->has('success_message'))
-            <div class="alert bg-success text-white">
-                {!! session()->get('success_message') !!}
-            </div>
-        @endif
-
-        @if (!site_setting('catalog_purchases_enabled') && Str::startsWith(request()->route()->getName(), 'catalog.'))
-            <div class="alert bg-warning text-center text-white" style="font-weight:600;">
-                Market purchases are temporarily unavailable. Items may be browsed but are unable to be purchased.
-            </div>
-        @endif
-
+@endif
+    @endguest
         @yield('content')
     </div>
 
@@ -350,11 +176,6 @@
           </div>
         </main>
       </nav>
-
-    <!-- JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script>
         var _token;
 
