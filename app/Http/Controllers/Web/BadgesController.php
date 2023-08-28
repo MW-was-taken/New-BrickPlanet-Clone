23339084fd9cf525524c7ddf2b8effe01ced12af
Trvvis <timemachinecorey@gmail.com>
2023-08-28 03:29:51 +0100

    added admin badge count to BadgeController

--- app/Http/Controllers/Web/BadgesController.php
+++ app/Http/Controllers/Web/BadgesController.php
@@ -1,32 +1,10 @@
 <?php
-/**
- * MIT License
- *
- * Copyright (c) 2021-2022 FoxxoSnoot
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 
 namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class BadgesController extends Controller
 {
@@ -44,8 +22,20 @@ class BadgesController extends Controller
             $badges[] = $badge;
         }
 
+        $adminBadgeCount = $this->adminBadgeCount();
+
         return view('web.badges.index')->with([
-            'badges' => $badges
+            'badges' => $badges,
+            'adminBadgeCount' => $adminBadgeCount,
         ]);
+    }
+
+    public function adminBadgeCount()
+    {
+        $adminBadgeCount = DB::table('user_badges')
+            ->where('badge_id', 1)
+            ->count();
+
+        return $adminBadgeCount;
     }
 }
